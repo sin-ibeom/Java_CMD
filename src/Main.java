c3cdf2fdd1a0b2d5a76b037c68071b1d56b0fb32
DESKTOP-DVACVAD\eric0 <eric091214@gmail.com>
2026-08-28 19:28:04 +0900

    ✨Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public static void main(){
+
+    Scanner sc = new Scanner(System.in);
+    File_main fm = new File_main();
+    while(true){
+        String input = sc.nextLine();
+        fm.input(input);
+    }
+
+
+
+
+
+}
